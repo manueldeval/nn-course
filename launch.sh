2d2c0d9100f670464a03ebda7c1284a2468d3cd6
Manuel Deval <manuel.deval@gmail.com>
2018-02-12 15:22:49 +0100

    Script helper

---
+++ launch.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Get the location of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WORK_VOLUME=$DIR/src
+MY_UID=$(id -u)
+
+if [ ! -d "$WORK_VOLUME" ]; then
+	mkdir $WORK_VOLUME
+fi
+docker run -it --rm -p 8888:8888 \
+	-e GRANT_SUDO=yes \
+	-e NB_UID=$MY_UID \
+	-v $WORK_VOLUME:/home/jovyan/work \
+	jupyter/tensorflow-notebook
+
